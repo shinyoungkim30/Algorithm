515dcda734cd9cd6c80839ce3d23cfc8ed6d4c4c
김신영 <ksyq12@gmail.com>
2023-12-28 20:21:28 +0900

    commit

---
+++ untitled/src/StackQueue/Ex05쇠막대기.java
@@ -0,0 +1,32 @@
+package untitled.src.StackQueue;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Ex05쇠막대기 {
+    public int solution(String str) {
+        int answer = 0;
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') stack.push('(');
+            else {
+                stack.pop();
+                if (str.charAt(i - 1) == '(') answer += stack.size();
+                else answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Ex05쇠막대기 T = new Ex05쇠막대기();
+
+        Scanner kb = new Scanner(System.in);
+
+        String str = kb.next();
+
+        System.out.println(T.solution(str));
+    }
+}
