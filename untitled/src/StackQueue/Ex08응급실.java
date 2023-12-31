4a0c31eec915e457224e49be3f28dd682bc58cb7
김신영 <ksyq12@gmail.com>
2023-12-31 20:08:08 +0900

    commit

--- untitled/src/StackQueue/Ex08응급실.java
+++ untitled/src/StackQueue/Ex08응급실.java
@@ -1,4 +1,60 @@
 package untitled.src.StackQueue;
 
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class Person {
+    int id;
+    int priority;
+
+    public Person(int id, int priority) {
+        this.id = id;
+        this.priority = priority;
+    }
+}
+
 public class Ex08응급실 {
+    public int solution(int n, int m, int[] arr) {
+        int answer = 0;
+
+        Queue<Person> Q = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            Q.add(new Person(i, arr[i]));
+        }
+
+        while (!Q.isEmpty()) {
+            Person tmp = Q.poll();
+            for (Person x : Q) {
+                if (x.priority > tmp.priority) {
+                    Q.offer(tmp);
+                    tmp = null;
+                    break;
+                }
+            }
+            if (tmp != null) {
+                answer++;
+                if (tmp.id == m) return answer;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Ex08응급실 T = new Ex08응급실();
+
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        System.out.println(T.solution(n, m, arr));
+    }
 }
