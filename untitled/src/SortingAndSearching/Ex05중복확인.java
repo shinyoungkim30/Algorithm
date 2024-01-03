e63e16d5377644fb5978c1fd4b1df521d40f3ebe
김신영 <ksyq12@gmail.com>
2024-01-04 07:45:24 +0900

    commit

---
+++ untitled/src/SortingAndSearching/Ex05중복확인.java
@@ -0,0 +1,32 @@
+package untitled.src.SortingAndSearching;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ex05중복확인 {
+    public String solution(int n, int[] arr) {
+        String answer = "U";
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] == arr[i + 1]) return "D";
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Ex05중복확인 T = new Ex05중복확인();
+
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
+
+        System.out.println(T.solution(n, arr));
+    }
+}
